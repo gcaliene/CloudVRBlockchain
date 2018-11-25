c214dc8b6a8de5524ff31eaaaf51b42c04437f65
Gerson C <gersoncalienes@gmail.com>
2018-11-25 16:23:55 -0500

    Made new class for tdd

---
+++ block-tdd.js
@@ -0,0 +1,10 @@
+class Block {
+//     constructor({ timestamp, lasthash, hash, data }){ //The advantage of wrapping it in a object block, is that you don't have to remember the order of the instances.
+//         this.timestamp = timestamp;
+//         this.lasthash = lasthash;
+//         this.hash = hash;
+//         this.data = data;
+//     }
+}
+
+module.exports = Block;
