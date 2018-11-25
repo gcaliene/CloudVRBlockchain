2c8f6858ef1a315960be4acd07db3f5cc4c25e5d
Gerson C <gersoncalienes@gmail.com>
2018-11-25 16:16:05 -0500

    the initial block class is now mapped out

--- block.js
+++ block.js
@@ -1,5 +1,5 @@
 class Block {
-    constructor(timestamp, lasthash, hash, data){
+    constructor({ timestamp, lasthash, hash, data }){ //The advantage of wrapping it in a object block, is that you don't have to remember the order of the instances.
         this.timestamp = timestamp;
         this.lasthash = lasthash;
         this.hash = hash;
@@ -7,6 +7,14 @@ class Block {
     }
 }
 
-const block1 = new Block('01/11/89', 'foo-lasthash', 'foo-hash', 'foo-data');
+//This is done without mapping, and will yield undefined if map is not specified
+// const block1 = new Block('01/11/89', 'foo-lasthash', 'foo-hash', 'foo-data');
+
+const block1 = new Block({
+    lasthash: 'foo-lasthash',
+    hash: 'foo-lasthash',
+    data: 'foo-data',
+    timestamp: '04/15/1989',
+})
 
 console.log('block1', block1)
