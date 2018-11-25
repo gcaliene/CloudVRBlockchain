543388758213a6a8ec2f66c7d870afc6bc8655ec
Gerson C <gersoncalienes@gmail.com>
2018-11-25 16:28:29 -0500

    Finished block.test.js

---
+++ block.test.js
@@ -0,0 +1,15 @@
+describe('Block', () => {
+    const timestamp = 'anydata';
+    const lastHash = 'foohash why not';
+    const hash =  'har-hash';
+    const data = ['blockchain', 'data'];
+    const block = new Block({ timestamp, lastHash, hash, data });
+
+    it('has a timestamp, lasthash, hash, and data property', () => {
+        expect(block.timestamp).toEqual(timestamp);
+        expect(block.lastHash).toEqual(lastHash);
+        expect(block.hash).toEqual(hash);
+        expect(block.data).toEqual(data);
+
+    })
+});
