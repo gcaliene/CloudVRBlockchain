a6b44aad740c298c6c4e3cc16be14a9dbe58f8b2
Gerson C <gersoncalienes@gmail.com>
2018-11-27 21:39:41 -0500

    Passing tests with genesis data

--- block-tdd.js
+++ block-tdd.js
@@ -1,9 +1,15 @@
+const {GENESIS_DATA} = require('./config');
+
 class Block {
     constructor({ timestamp, lastHash, hash, data }){ //The advantage of wrapping it in a object block, is that you don't have to remember the order of the instances.
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+    }
+
+    static genesis(){
+        return new Block(GENESIS_DATA)
     }
 }
 
