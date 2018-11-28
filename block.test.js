c187dbf38dde5f8bd00371305c6e6889b4c14f8e
Gerson C <gersoncalienes@gmail.com>
2018-11-27 21:28:21 -0500

    Set the test to run for genesis()

--- block.test.js
+++ block.test.js
@@ -1,5 +1,5 @@
 const Block = require('./block-tdd') //.js is implicit specified by node.js
-
+const {GENESIS_DATA} = require('./config')
 
 describe('Block', () => {
     const timestamp = 'anydata';
@@ -13,6 +13,15 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+    })
 
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis(); //static function 
+        it('returns a block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        })
+        it('returns the genesis data', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        })
     })
 });
