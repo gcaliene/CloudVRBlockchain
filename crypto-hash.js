89a05be7d81a0a47151af9534f6b9247343d0546
Gerson C <gersoncalienes@gmail.com>
2018-12-02 21:01:58 -0500

    cryptohash functions working

--- crypto-hash.js
+++ crypto-hash.js
@@ -3,6 +3,10 @@ const crypto = require('crypto'); //can create a powerful Hash object
 const cryptoHash = (...inputs) => {  //Since we can't guess how many arguments are there going to be coming in, we have to use the JS spread operator.
     const hash = crypto.createHash('sha256');
     
+    hash.update(inputs.sort().join(' ')); //sort() so that the order doesnt matter
+
+    return hash.digest('hex');
+
 }
 
 module.exports = cryptoHash;
