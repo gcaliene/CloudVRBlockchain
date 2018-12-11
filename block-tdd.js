df00582aa0fa497f3e2171bff77e928aa8060e86
Gerson C <gersoncalienes@gmail.com>
2018-12-10 20:05:54 -0500

    finished test for mineblock

--- block-tdd.js
+++ block-tdd.js
@@ -1,4 +1,5 @@
 const {GENESIS_DATA} = require('./config');
+const cryptoHash = require('./crypto-hash')
 
 class Block {
     constructor({ timestamp, lastHash, hash, data }){ //The advantage of wrapping it in a object block, is that you don't have to remember the order of the instances.
@@ -15,10 +16,14 @@ class Block {
     }
     
     static mineBlock({ lastBlock, data }) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+
         return new this({
             timestamp: Date.now(),
             lastHash:lastBlock.hash,
-            data
+            data,
+            hash: cryptoHash(timestamp, lastHash, data)
         })
     }
 }
