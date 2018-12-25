d237932688164b908edc726e7fd52cf9b88ef8e3
Gerson C <gersoncalienes@gmail.com>
2018-12-25 11:08:51 -0500

    Added blockchain jest test cases

--- blockchain.test.js
+++ blockchain.test.js
@@ -14,25 +14,39 @@ describe('Blockchain', () => {
 
     it('adds a new block to the chain', () => {
         const newData = 'foo bar';
-        blockchain.addBlock({ data: newData });
+        blockchain.addBlock({
+            data: newData
+        });
 
-        expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData);
+        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
     })
 
     describe('isValidChain()', () => {
         describe('when the chain does not start with the genesis block', () => {
-            it('returns false', () => {})
+            it('returns false', () => {
+                blockchain.chain[0] = {
+                    data: 'fake-genesis'
+                };
+                expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
+            })
         })
 
         describe('when the chain starts with the genesis block and has multiple blocks', () => {
             describe('and a lastHash Reference has changed', () => {
                 it('returns false', () => {
+                    blockchain.addBlock({ data: 'Bees'})
+                    blockchain.addBlock({ data: 'Banjos'})
+                    blockchain.addBlock({ data: 'Schrute'})
+
+                    blockchain.chain[2].lastHash = 'broken-lastHash';
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false)
 
                 })
             })
 
             describe('and the chain contains a block with an invalid field', () => {
-                it( 'returns false', () => {
+                it('returns false', () => {
 
                 })
             })
@@ -45,4 +59,3 @@ describe('Blockchain', () => {
         })
     })
 })
-
