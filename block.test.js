9d56898c361a730fd9fce67c270c77f82a54be88
Gerson C <gersoncalienes@gmail.com>
2019-01-05 22:27:53 -0500

    UPDATE: now includes nonce and difficulty in tests of Block

--- block.test.js
+++ block.test.js
@@ -9,11 +9,15 @@ describe('Block', () => {
     const lastHash = 'foohash why not';
     const hash = 'har-hash';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
     const block = new Block({
         timestamp,
         lastHash,
         hash,
-        data
+        data,
+        nonce,
+        difficulty
     });
 
     it('has a timestamp, lasthash, hash, and data property', () => {
@@ -21,6 +25,8 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     })
 
     describe('genesis()', () => {
@@ -60,8 +66,21 @@ describe('Block', () => {
         })
 
         it('creates a SHA-256 `hash` based on the proper inputs', () => {
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data))
+            expect(minedBlock.hash)
+                .toEqual(
+                    cryptoHash(
+                        minedBlock.timestamp,
+                        minedBlock.nonce,
+                        minedBlock.difficulty,
+                        lastBlock.hash,
+                        data
+                    )
+                );
         })
 
+        it('sets the `hash` that matches the difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty))
+        })
     })
 });
