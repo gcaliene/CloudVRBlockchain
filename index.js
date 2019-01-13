9c6f968eba7ea24e65e6fdd4199886a027ba9691
Gerson C <gersoncalienes@gmail.com>
2019-01-12 21:29:53 -0500

    Using express.json instead of bodyParser

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
 const app = express();
 const blockchain = new Blockchain();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+    const { data } = req.body;
+
+    blockchain.addBlock({ data });
+
+    res.redirect('/api/blocks');
 })
 
 app.listen(PORT, () => {
     console.log('The app is running on PORT:', PORT)
-})
+});
