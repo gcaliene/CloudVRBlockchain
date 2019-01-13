d5da2cdc2e0b7839de1821bbad005d7bea9e4e49
Gerson C <gersoncalienes@gmail.com>
2019-01-12 20:46:01 -0500

    Added check with lastDifficulty

--- blockchain.js
+++ blockchain.js
@@ -37,16 +37,21 @@ class Blockchain {
             return false;
         } 
 
+
         // i=1 bc we want to skip the genesis block as it is already validated
         for(let i = 1; i<chain.length; i++) {
             const block = chain[i];
             const actualLastHash = chain[i-1].hash;
             const { timestamp, lastHash, hash, nonce, difficulty, data } = block;
+            const lastDifficulty = chain[i-1].difficulty;
+
 
             if (lastHash !== actualLastHash) return false;
 
             const validatedHash = cryptoHash(timestamp, lastHash, nonce, difficulty, data);
             if (hash !== validatedHash) return false;
+
+            if(Math.abs(lastDifficulty - difficulty) > 1) return false;
         }
 
         return true;
