e7c508593a2f92f236ede9036d6f993546b7edb2
Gerson C <gersoncalienes@gmail.com>
2019-01-13 16:56:59 -0500

    Added 4 helper methods:
    handleMessage, subscribeToChannels, publish, broadcastChain

--- pubsub.js
+++ pubsub.js
@@ -1,24 +1,54 @@
 const redis = require('redis');
 
 const CHANNELS = {
-    TEST: 'TEST'
+    TEST: 'TEST',
+    BLOCKCHAIN: 'BLOCKCHAIN'
 };
 
 class PubSub {
-    constructor() {
+    constructor({ blockchain }) {
+        this.blockchain = blockchain;
+
         this.publisher = redis.createClient();
         this.subscriber = redis.createClient();
 
-        this.subscriber.subscribe(CHANNELS.TEST);
+        this.subscribeToChannels();
 
-        this.subscriber.on('message', (channel, message) => this.handleMessage(channel, message));
+        this.subscriber.on(
+            'message', 
+            (channel, message) => this.handleMessage(channel, message)
+        );
     }
 
     handleMessage(channel, message){
-        console.log(`Message received. Channel: ${channel}. Message: ${message}.`)
+        console.log(`Message received. Channel: ${channel}. Message: ${message}.`);
+        const parsedMessage = JSON.parse(message);
+        
+        if (channel === CHANNELS.BLOCKCHAIN) {
+            this.blockchain.replaceChain(parsedMessage);
+        }
+    }
+
+    subscribeToChannels() {
+        Object.values(CHANNELS).forEach(channel => {
+            this.subscriber.subscribe(channel);
+        })
+    }
+
+    publish({ channel, message }) {
+        this.publisher.publish(channel, message);
+    }
+
+    broadcastChain() {
+        this.publish({
+            channel: CHANNELS.BLOCKCHAIN,
+            message: JSON.stringify(this.blockchain.chain)
+        })
     }
 }
 
-const testPubSub = new PubSub();
+module.exports = PubSub;
 
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 10);
+// const testPubSub = new PubSub();
+
+// setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 10);
