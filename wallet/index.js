ce5ebce0bdc38b3e3f985d71efcad7ddec60fa66
Gerson C <gersoncalienes@gmail.com>
2019-01-15 22:52:15 -0500

    Incorporate ec to wallet

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,16 @@
 const { STARTING_BALANCE } = require('../config');
+const { ec } = require('../util');
 
 class Wallet {
     constructor(){
         this.balance = STARTING_BALANCE;
+
+        const keyPair = ec.genKeyPair();
+
+        //publicKey is the key used as an address not a signature
+        this.publicKey = keyPair.getPublic().encode('hex');
+
+        //privateKey is the key used as a signature 
     }
 
 };
