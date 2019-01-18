faa15780bcd503b101095f84f9c1dea44c23173e
Gerson C <gersoncalienes@gmail.com>
2019-01-17 22:15:29 -0500

    verifySignature function added

--- util/index.js
+++ util/index.js
@@ -1,5 +1,11 @@
 const EC = require('elliptic').ec;
+const cryptoHash = require('./crypto-hash')
 
 const ec = new EC('secp256k1');
 
-module.exports = { ec };
+const verifySignature = ({ publicKey, data, signature }) => {
+    const keyFromPublic = ec.keyFromPublic(publicKey, 'hex'); //Have to let them know that we are using cryptohash which is hex
+    return keyFromPublic.verify(cryptoHash(data), signature);
+};
+
+module.exports = { ec, verifySignature };
