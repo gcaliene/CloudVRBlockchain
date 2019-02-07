1a7ef664f9553c4887745fb56e8dda94a6da9c17
Gerson C <gersoncalienes@gmail.com>
2019-02-06 21:14:52 -0500

    create input test cases for transaction class

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -28,5 +28,23 @@ describe('Transaction', () => {
         it('outputs the remaining balance for the `senderWallet`', () => {
             expect(transaction.outputMap[senderWallet.publicKey]).toEqual(senderWallet.balance - amount);
         })
-    })
-})
+    });
+
+    describe('input', () => {
+        it('has an `input`', () => {
+            expect(transaction).toHaveProperty('input');
+        });
+
+        it('has a `timestamp` in the input', () => {
+            expect(transaction.input).toHaveProperty('timestamp');
+        });
+
+        it('sets the `amount` to the `senderWallet` balance', () => {
+            expect(transaction.input.amount).toEqual(senderWallet.balance);
+        });
+
+        it('sets the `address` to the `senderWallet` publicKey', () => {
+            expect(transaction.input.address).toEqual(senderWallet.publicKey);
+        });
+    });
+});
