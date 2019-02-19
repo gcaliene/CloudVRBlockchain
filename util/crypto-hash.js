38f40f091eb6b1341da8641d402f61c830056f73
Windows <gersoncalienes@gmail.com>
2019-02-19 11:43:25 -0500

    Stringified ...input in order to produce unique signature with an updated transaction

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -3,7 +3,14 @@ const crypto = require('crypto'); //can create a powerful Hash object
 const cryptoHash = (...inputs) => {  //Since we can't guess how many arguments are there going to be coming in, we have to use the JS spread operator.
     const hash = crypto.createHash('sha256');
     
-    hash.update(inputs.sort().join(' ')); //sort() so that the order doesnt matter
+    //Stringifying the input allows the signature to change when the transaction updates.
+    hash
+        .update(
+            inputs
+                .map(input => JSON.stringify(input)) //Stringifying the input allows the signature to change when the transaction updates.
+                .sort()
+                .join(' ')
+        ); //sort() so that the order doesnt matter
 
     return hash.digest('hex');
 
