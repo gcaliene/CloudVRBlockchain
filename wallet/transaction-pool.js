fd51fb5cfe452a2930b3ba3d26adeb8f7046cdc1
Gerson C <gersoncalienes@gmail.com>
2019-02-21 21:59:15 -0500

    Added existingTransaction() function

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -6,6 +6,13 @@ class TransactionPool {
   setTransaction(transaction) {
     this.transactionMap[transaction.id] = transaction;
   }
+
+  //without this, new transactions will keep piling up
+  existingTransaction({ inputAddress }) {
+    const transactions = Object.values(this.transactionMap);
+
+    return transactions.find(transaction => transaction.input.address === inputAddress);
+  }
 }
 
-module.exports = TransactionPool
+module.exports = TransactionPool;
