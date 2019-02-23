24e2a63ee947115bf9d4f838c55ad7725ffbbced
Windows <gersoncalienes@gmail.com>
2019-02-22 19:59:20 -0500

    Validate transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,5 @@
+const Transaction = require('./transaction');
+
 class TransactionPool {
   constructor() {
     this.transactionMap = {}
@@ -13,11 +15,20 @@ class TransactionPool {
   }
 
   //without this, new transactions will keep piling up
-  existingTransaction({ inputAddress }) {
+  existingTransaction({
+    inputAddress
+  }) {
     const transactions = Object.values(this.transactionMap);
 
     return transactions.find(transaction => transaction.input.address === inputAddress);
   }
+
+  validTransactions() {
+    return Object.values(this.transactionMap).filter(
+      transaction => Transaction.validTransaction(transaction)
+    )
+  }
+
 }
 
 module.exports = TransactionPool;
